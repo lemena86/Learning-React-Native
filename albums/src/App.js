0641020bcc854f89b6ae421c52aed76419566d1a
Luis E Mena <lemena86@gmail.com>
2018-01-09 23:33:10 -0300

    [albums] header components

---
+++ albums/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import Header from './components/Header';
+
+const App = () => (
+  <View>
+    <Header headerText="Albums!" />
+  </View>
+);
+
+export default App;
